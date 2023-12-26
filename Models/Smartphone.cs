a99414c7b47a2874e5ac87e8fac14d65001b3558
Lucemila Bonifácio dos Santos <lucemaylabb@gmail.com>
2023-12-26 17:01:08 -0300

    Desafio concluído git add .git add .git add .

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,15 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +25,32 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+    }
+ public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar lógica específica do construtor Nokia, se necessário
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Nokia...");
+           
+        }
+    }
+
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar lógica específica do construtor iPhone, se necessário
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 8", "N123456", 64);
+            Console.WriteLine($"Número: {nokia.Numero}");
+            Console.WriteLine($"Modelo: {nokia.Modelo}");
+            Console.WriteLine($"IMEI: {nokia.IMEI}");
+            Console.WriteLine($"Memória: {nokia.Memoria}GB");
+
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+           
+            Iphone iphone = new Iphone("987654321", "iPhone 12", "I987654", 128);
+            Console.WriteLine($"Número: {iphone.Numero}");
+            Console.WriteLine($"Modelo: {iphone.Modelo}");
+            Console.WriteLine($"IMEI: {iphone.IMEI}");
+            Console.WriteLine($"Memória: {iphone.Memoria}GB");
+
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
